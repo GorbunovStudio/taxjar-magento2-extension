85ce3ae3aab87152eff325c32901fb2949cf2620
Kochnov Denis <d.kochnov.optimus@gmail.com>
2025-08-18 14:03:00 +0300

    Refs #35129: Fix extension attributes const names

--- Api/Data/Sales/Order/ExtensionAttributesInterface.php
+++ Api/Data/Sales/Order/ExtensionAttributesInterface.php
@@ -6,5 +6,5 @@ interface ExtensionAttributesInterface
 {
     const TJ_TAX_CALCULATION_STATUS = 'tj_tax_calculation_status';
     const TJ_TAX_CALCULATION_MESSAGE = 'tj_tax_calculation_message';
-    const PREVENT_TAX_SYNC = 'tj_tax_prevent_tax_sync';
+    const TJ_TAX_PREVENT_TAX_SYNC = 'tj_tax_prevent_tax_sync';
 }
--- Api/Data/Sales/Quote/ExtensionAttributesInterface.php
+++ Api/Data/Sales/Quote/ExtensionAttributesInterface.php
@@ -4,5 +4,5 @@ namespace Taxjar\SalesTax\Api\Data\Sales\Quote;
 
 interface ExtensionAttributesInterface
 {
-    const FORCE_MAGENTO_TAX_COLLECT = 'tj_tax_force_magento_tax_collect';
+    const TJ_TAX_FORCE_MAGENTO_TAX_COLLECT = 'tj_tax_force_magento_tax_collect';
 }
