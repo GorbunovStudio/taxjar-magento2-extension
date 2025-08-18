38fb5b33546006bad3e03f50e472e354b352a8e0
Kochnov Denis <d.kochnov.optimus@gmail.com>
2025-08-18 12:40:59 +0300

    Refs #35127: Add extension attributes for Order and Quote interfaces

---
+++ Api/Data/Sales/Order/ExtensionAttributesInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Taxjar\SalesTax\Api\Data\Sales\Order;
+
+interface ExtensionAttributesInterface
+{
+    const TJ_TAX_CALCULATION_STATUS = 'tj_tax_calculation_status';
+    const TJ_TAX_CALCULATION_MESSAGE = 'tj_tax_calculation_message';
+    const PREVENT_TAX_SYNC = 'tj_tax_prevent_tax_sync';
+}
---
+++ Api/Data/Sales/Quote/ExtensionAttributesInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Taxjar\SalesTax\Api\Data\Sales\Quote;
+
+interface ExtensionAttributesInterface
+{
+    const FORCE_MAGENTO_TAX_COLLECT = 'tj_tax_force_magento_tax_collect';
+}
